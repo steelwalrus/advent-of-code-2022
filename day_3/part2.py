413b2c6cc658e4d646a1a6fc147d8f873c8415db
Jay Turner <jayturner1832@gmail.com>
2022-12-03 23:00:47 +0000

    day 3 part 2

--- day_3/part2.py
+++ day_3/part2.py
@@ -8,6 +8,7 @@ class Rucksack:
     def __init__(self, items):
 
         half_of_items = len(items) // 2
+        self.contents = items
         self.compartment_a = items[:half_of_items]
         self.compartment_b = items[half_of_items:]
         self.shared = []
@@ -21,12 +22,24 @@ class Rucksack:
 
 
 priority_sum = 0
+size_of_group = 3
 
-for c in inputs:
-    r = Rucksack(c)
-    value = r.get_item_priority(r.get_shared_item())
-    priority_sum += value
+for group in zip(*(iter(i for i in inputs),) * size_of_group):
+    tally = {}
+
+    for i, bag_contents in enumerate(group):
+        r = Rucksack(bag_contents)
+        unique_contents = set(r.contents)
+
+        for item in unique_contents:
+            if item not in tally:
+                tally[item] = 1
+            else:
+                tally[item] += 1
+                if tally[item] == 3:
+                    priority_sum += r.get_item_priority(item)
+                    break
 
 print(priority_sum)
 
-# 7793
+# 2499

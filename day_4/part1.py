9f4423936453bd56ea7bc2330224e29cb3c39fed
Jay Turner <jayturner1832@gmail.com>
2022-12-04 08:22:26 +0000

    day 4 parts 1 and 2

---
+++ day_4/part1.py
@@ -0,0 +1,24 @@
+with open('day_4/input.txt', 'r') as file:
+    f = file.read().strip()
+    inputs = f.splitlines()
+
+
+overlaps = 0
+
+for pair in inputs:
+    split_pair = pair.split(',')
+    pair1 = split_pair[0].split('-')
+    pair2 = split_pair[1].split('-')
+
+    work_area_1 = [i for i in range(int(pair1[0]), int(pair1[1]) + 1)]
+    work_area_2 = [i for i in range(int(pair2[0]), int(pair2[1]) + 1)]
+
+    intersection = set(work_area_1).issubset(work_area_2) or set(work_area_1).issuperset(work_area_2)
+
+    if intersection:
+        overlaps += 1
+
+print(overlaps)
+
+# 530
+
